972a81afb8b8648b628943cca4010ab136d241e6
shirelazulay <10shirel@gmail.com>
2025-03-05 02:44:13 +0200

    Add the ability to upload to S3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Our.Umbraco.StorageProviders.AWSS3.DependencyInjection;  // זה המרחב השמות הנכון
+using Umbraco.Cms.Web.Common.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
     .AddDeliveryApi()
     .AddComposers()
+    // Add the AWS S3 Storage file system
+    .AddAWSS3MediaFileSystem()
     .Build();
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 await app.BootUmbracoAsync();
-
 
 app.UseUmbraco()
     .WithMiddleware(u =>
     {
         u.UseBackOffice();
         u.UseWebsite();
+        // Enables the AWS S3 Storage middleware for Media
+        u.UseAWSS3MediaFileSystem();
     })
     .WithEndpoints(u =>
     {
